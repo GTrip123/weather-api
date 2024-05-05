a7b99329bf6d1fb72c7909f7d50bcc6e3882dce2
GTrip123 <108158973+GTrip123@users.noreply.github.com>
2024-05-05 12:29:26 -0500

    weather.py

---
+++ weather.py
@@ -0,0 +1,17 @@
+pip install requests
+import requests
+api_key = 'c6fc8fef116fad40bafa82a02ebdff74'
+city = input('Pensacola')
+
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
